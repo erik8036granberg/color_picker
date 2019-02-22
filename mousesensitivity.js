89e5a98bb467e277d4b641efc655511fa6392eee
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-02-22 21:33:33 +0100

    Step 1

--- mousesensitivity.js
+++ mousesensitivity.js
@@ -25,13 +25,46 @@ function crateAndLoadImage() {
 }
 
 function getImageData() {
+    console.log("getImageData");
     // get pixel data from image
     imageData = ctx.getImageData(0, 0, imageWidth, imageHeight);
     console.log(imageData);
 }
 
 function drawImage() {
+    console.log("drawImage");
     // draw image in canvas
     ctx.drawImage(newImage, 0, 0);
     getImageData();
+}
+
+// get mouse position move on canvas
+imageCanvas.addEventListener("mousemove", getPosition, false);
+
+function getPosition(event) {
+    console.log("getPosition");
+
+    // clear canvas - clearRect + canvas size
+    ctx.clearRect(0, 0, imageWidth, imageHeight);
+
+    // get mouse position
+    let x = event.x;
+    let y = event.y;
+    x -= imageCanvas.offsetLeft;
+    y -= imageCanvas.offsetTop;
+
+    console.log("x:" + x + " y:" + y);
+
+    // put image data in ctx
+    ctx.putImageData(imageData, 0, 0);
+
+    drawRectangle(x, y);
+}
+
+function drawRectangle(x, y) {
+    console.log("drawRectangle");
+    // draw rectangle with 5 px offset for centering
+    ctx.strokeRect(x - 5, y - 5, 10, 10);
+    ctx.strokeStyle = "green";
+    ctx.moveTo = (x, y);
 }
