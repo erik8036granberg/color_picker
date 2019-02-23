34bd7a506d542ffc9e9371734c5819dd69b7a8b1
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-02-23 19:49:20 +0100

    displacement - image size check & imageData x3 ok

--- displacement-map/displacement-map.js
+++ displacement-map/displacement-map.js
@@ -6,12 +6,17 @@ const ctx = imageCanvas.getContext("2d");
 let imageData;
 const imageWidth = ctx.canvas.width;
 const imageHeight = ctx.canvas.height;
+
 const mapImage = new Image();
 const mapCanvas = document.querySelector("#mapCanvas");
-const ctxMap = mapCanvas.getContext("2d");
+const mapCtx = mapCanvas.getContext("2d");
 let mapData;
-const mapWidth = ctxMap.canvas.width;
-const mapHeight = ctxMap.canvas.height;
+const mapWidth = mapCtx.canvas.width;
+const mapHeight = mapCtx.canvas.height;
+
+const outputCtx = outputCanvas.getContext("2d");
+const outputmWidth = outputCtx.canvas.width;
+let outputData;
 
 window.addEventListener("DOMContentLoaded", init);
 
@@ -22,33 +27,50 @@ function init() {
 
 function loadImages() {
   console.log("loadImages");
+
   // image path for original image
   newImage.src = "cat.jpg";
 
   // image path for map image
   mapImage.src = "map.jpg";
 
-  // check if images is loaded
-  if (!newImage.complete && !mapImage.complete) {
-    console.log("images loaded");
-    newImage.addEventListener("load", drawImages);
+  // check if the image sizes are the same
+  if (
+    newImage.naturalWidth !== mapImage.naturalWidth ||
+    newImage.naturalHeight !== mapImage.naturalHeight
+  ) {
+    alert("WTF! - The images have different sizes??");
   }
+
+  // todo - find preload-stuff that works
+  newImage.addEventListener("load", drawImages);
 }
 
 function drawImages() {
   console.log("drawImages");
+
   // draw original image in canvas
   ctx.drawImage(newImage, 0, 0);
+
   // draw map image in canvas
-  ctxMap.drawImage(mapImage, 0, 0);
+  mapCtx.drawImage(mapImage, 0, 0);
+
   getImageData();
 }
 
 function getImageData() {
   console.log("getImageData");
+
   // get pixel data from image
   imageData = ctx.getImageData(0, 0, imageWidth, imageHeight);
-  mapData = ctxMap.getImageData(0, 0, mapWidth, mapHeight);
+  mapData = mapCtx.getImageData(0, 0, mapWidth, mapHeight);
   console.log(imageData);
   console.log(mapData);
+}
+
+function createOutputData() {
+  console.log(outputData);
+  // create an empty imageData for the zoom canvas
+  console.log("createOutputData");
+  outputData = outputCtx.createImageData(500, 600);
 }
