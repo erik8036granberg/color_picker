12af9c2030ce126d29d9253522279212a856404d
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-02-23 23:12:31 +0100

    copy to outputcanvas OK

--- displacement-map/displacement-map.js
+++ displacement-map/displacement-map.js
@@ -11,11 +11,8 @@ const mapImage = new Image();
 const mapCanvas = document.querySelector("#mapCanvas");
 const mapCtx = mapCanvas.getContext("2d");
 let mapData;
-const mapWidth = mapCtx.canvas.width;
-const mapHeight = mapCtx.canvas.height;
 
 const outputCtx = outputCanvas.getContext("2d");
-const outputmWidth = outputCtx.canvas.width;
 let outputData;
 
 window.addEventListener("DOMContentLoaded", init);
@@ -41,9 +38,9 @@ function loadImages() {
   ) {
     alert("WTF! - The images have different sizes??");
   }
-
-  // todo - find preload-stuff that works
-  newImage.addEventListener("load", drawImages);
+  newImage.addEventListener("load", function() {
+    mapImage.addEventListener("load", drawImages);
+  });
 }
 
 function drawImages() {
@@ -55,22 +52,100 @@ function drawImages() {
   // draw map image in canvas
   mapCtx.drawImage(mapImage, 0, 0);
 
-  getImageData();
-}
-
-function getImageData() {
-  console.log("getImageData");
-
-  // get pixel data from image
+  // save pixel data from image
   imageData = ctx.getImageData(0, 0, imageWidth, imageHeight);
-  mapData = mapCtx.getImageData(0, 0, mapWidth, mapHeight);
-  console.log(imageData);
-  console.log(mapData);
-}
+  mapData = mapCtx.getImageData(0, 0, imageWidth, imageHeight);
 
-function createOutputData() {
-  console.log(outputData);
-  // create an empty imageData for the zoom canvas
-  console.log("createOutputData");
+  // create an empty imageData for the output canvas
   outputData = outputCtx.createImageData(500, 600);
 }
+
+// get mouse position on every move on the canvas
+imageCanvas.addEventListener("mousemove", getPosition, false);
+
+function getPosition(event) {
+  console.log("getPosition");
+
+  // clear canvas - clearRect + canvas size
+  ctx.clearRect(0, 0, imageWidth, imageHeight);
+
+  // get mouse position
+  let x = event.x;
+  let y = event.y;
+  x -= imageCanvas.offsetLeft;
+  y -= imageCanvas.offsetTop;
+
+  // put image data in ctx
+  ctx.putImageData(imageData, 0, 0);
+  copyPixel(x, y);
+  calculateRatios(x, y);
+}
+
+function calculateRatios(x, y) {
+  // -1 to 1 ratio (x / imageWidth * range - center )
+  const mouseXratio = (x / imageWidth) * 2 - 1;
+  const mouseYratio = (y / imageHeight) * 2 - 1;
+
+  document.querySelector("#pos_x_ratio").textContent = mouseXratio;
+  document.querySelector("#pos_y_ratio").textContent = mouseYratio;
+
+  drawRectangle(x, y, mouseXratio, mouseYratio);
+}
+
+function drawRectangle(x, y, mouseXratio, mouseYratio) {
+  console.log("drawRectangle");
+
+  // displacementX/Y
+  const MAX_MOVEMENT = 20; // max movement i px
+  const displacementX = MAX_MOVEMENT * mouseXratio;
+  const displacementY = MAX_MOVEMENT * mouseYratio;
+
+  //   Rectangle size
+  const rectWidth = imageWidth - MAX_MOVEMENT;
+  const rectHeight = imageHeight - MAX_MOVEMENT;
+
+  console.log(MAX_MOVEMENT);
+  console.log(`disX: ${displacementX}`);
+  console.log(`disY: ${displacementY}`);
+
+  //   draw Rectangle with displacementX/Y "offset" + half of MAX_MOVEMENT
+  ctx.strokeRect(
+    displacementX + MAX_MOVEMENT / 2,
+    displacementY + MAX_MOVEMENT / 2,
+    rectWidth,
+    rectHeight
+  );
+
+  ctx.strokeStyle = "green";
+  ctx.moveTo = (x, y);
+}
+
+function copyPixel(startX, startY) {
+  console.log("copyPixel");
+
+  // loop for x and y content i output Canvas
+  for (let y = 0; y < imageHeight; y++) {
+    for (let x = 0; x < imageWidth; x++) {
+      // calculate pixelIndex position (loop x & y)
+      const pixelIndex = (x + y * imageWidth) * 4;
+
+      // copy each pixel (4 colors) data from imageData to zoomData
+      outputData.data[pixelIndex + 0] = imageData.data[pixelIndex + 0];
+      outputData.data[pixelIndex + 1] = imageData.data[pixelIndex + 1];
+      outputData.data[pixelIndex + 2] = imageData.data[pixelIndex + 2];
+      outputData.data[pixelIndex + 3] = imageData.data[pixelIndex + 3];
+    }
+    drawOutputData();
+  }
+}
+
+function drawOutputData() {
+  // put outputData in canvas
+  outputCtx.putImageData(outputData, 0, 0);
+}
+
+function loop() {}
+
+function uddate() {}
+
+function output() {}
